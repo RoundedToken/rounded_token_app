e22d5533779fa8470bae6746d0b5d2f10bc49ea3
RoundedToken <117864556+RoundedToken@users.noreply.github.com>
2023-04-12 21:11:27 +0200

    123

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'https://rounded-token.onrender.com/api';
+export const API_URL = process.env.REACT_APP_SERVER_URL;
 
 const $api = axios.create({
     withCredentials: true,
